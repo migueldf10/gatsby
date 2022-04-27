d9f9e02dbb1f89fe9555a5d18bf987ed80c7ca4f
veryspry <ehlinger.matt@gmail.com>
2022-04-27 17:56:32 +0100

    refactor: gatsbyImageResolver -> gatsbyImageSourceUrls

--- packages/gatsby/src/redux/types.ts
+++ packages/gatsby/src/redux/types.ts
@@ -968,7 +968,7 @@ export interface IProcessGatsbyImageSourceUrlAction {
 }
 
 export interface ITelemetry {
-  gatsbyImageResolver: Set<string>
+  gatsbyImageSourceUrls: Set<string>
 }
 
 export interface IMergeWorkerQueryState {
